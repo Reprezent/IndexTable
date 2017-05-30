32838cf864871dc41a4e3fd10d05f34256b20240
Richard Riedel <richardalexriedel@yahoo.com>
2017-05-30 12:38:44 -0400

    Add files via upload

---
+++ index_table.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <set>
+#include <tuple>
+#include <map>
+
+#define get_hash std::get<0>
+#define get_key std::get<1>
+#define get_value std::get<2>
+
+
--- index_table.hpp
+++ index_table.hpp
@@ -14,16 +14,26 @@
 
 
 #include <vector>
+#include <set>
+#include <tuple>
 #include <map>
+
+//using get_hash = std::get<0>;
+//using get_key = std::get<1>;
+//using get_value = std::get<2>;
 
 #define INITAL_TABLE_SIZE 1009
 
-// The dictionary class provides a key, value pair 
+// The dictionary class provides a key, value pairing that runs in constant time
+// for lookups.
 
 template <typename T, typename V>
+
+//#define std::tuple<int, T, V> dictionary_data
+
 class dictionary
 {
-	public;
+	public:
 		// Default constructor
 		//	- Allocates the index table and the vector to hold it
 		//	  the tuples of (hashValue, Key, Value).
@@ -64,6 +74,10 @@ class dictionary
 		//   - If the key is not within the dictionary then null is returned. 
 		// *********************** NEED TO CHANGE THIS TO A NEW VALUE OR SOMETHING, CAN'T DO IT IN C++ ********************
 		bool insert(T key, V value);
+		
+		// Inserts the key with the target value if th ekey has no value and
+		// returns the set value.
+		V insertIfAbsent(T key, V value);
 
 		// Returns true if the hashtable is empty.
 		bool empty();
@@ -77,20 +91,35 @@ class dictionary
 		// Returns true if the table contains the value.
 		//  - This function runs in linear time.
 		bool containsValue(V value);
+
+		// Returns a copy of the values in order that the keys were inserted,
+		// if none were updated.
 		std::vector<V>& elements();
+
+		// Returns a set of key -> value pairing in the order that the keys were addedto the dictionary.
 		std::set< std::pair<T, V> > entrySet();
-		boolean equals(const dictionary& table);
+
+		// Compares the two vectors for complete equality.
+		bool equals(const dictionary& table);
 
 
+		// Returns the value of the specified key if it exists, 
 		V get(T key);
 
 
 	private:
-		void __resize();
-		int __hash(T item);
-		
 
+		// Resizes th einternal datastructures that contain the mapping of th eindicies of the hashed values.
+		void __resize();
+
+		// Hash function that hashes the specified key.
+		int __hash(T item);
+		 
+
+		// Data structure that holdsthe indicies of the keys to the actaul key value mappings.
 		std::vector<unsigned int> indicies;
-		std::vector< std::tuple<size_t, T, V> > table;
+
+		// The actual key value mappings to for use from the indicies table.
+		std::vector< std::tuple<int, T, V> > table;
 
 };
